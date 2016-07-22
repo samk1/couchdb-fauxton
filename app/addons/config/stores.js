d6445f1b8e37daaceda29e747af8a2ca228180d9
samk <samkidman@gmail.com>
2016-07-22 21:16:50 +1000

    remove dead code

--- app/addons/config/stores.js
+++ app/addons/config/stores.js
@@ -15,45 +15,19 @@ function unset(object, sectionName, optionName) {
   }
 }
 
-function get(object, sectionName, optionName) {
-  if (object[sectionName]) {
-    return object[sectionName][optionName];
-  }
-}
-
 var ConfigStore = FauxtonAPI.Store.extend({
   initialize: function () {
     this.reset();
   },
 
   reset: function () {
-    this._node = null;
     this._sections = [];
     this._loading = true;
-    this._adding = false;
-    this._addOptionPopoverVisible = false;
-    this._editing = {};
-    this._saving = {};
-    this._deleting = {};
-    this._newSectionName = '';
-    this._newOptionName = '';
-    this._newOptionValue = '';
-    this._shouldUpdate = true;
   },
 
-  editConfig: function (sections, node) {
-    this._node = node;
+  editConfig: function (sections) {
     this._sections = sections;
     this._loading = false;
-    this._shouldUpdate = true;
-  },
-
-  shouldConfigTableUpdate: function () {
-    return this._shouldUpdate;
-  },
-
-  getNode: function () {
-    return this._node;
   },
 
   getSections: function () {
@@ -64,66 +38,12 @@ var ConfigStore = FauxtonAPI.Store.extend({
     return this._loading;
   },
 
-  isAdding: function () {
-    return this._adding;
-  },
-
-  isAddOptionPopoverVisible: function () {
-    return this._addOptionPopoverVisible;
-  },
-
-  getNewSectionName: function () {
-    return this._newSectionName;
-  },
-
-  getNewOptionName: function () {
-    return this._newOptionName;
-  },
-
-  getNewOptionValue: function () {
-    return this._newOptionValue;
-  },
-
-  editOption: function (sectionName, optionName) {
-    this._editing = {};
-    set(this._editing, sectionName, optionName);
-  },
-
-  setOptionValue: function (sectionName, optionName, newValue) {
-    set(this._sections, sectionName, optionName, newValue);
-  },
-
-  stopOptionEdit: function (sectionName, optionName) {
-    this._editing = {};
-    this._saving = {};
-  },
-
-  isOptionEditing: function (sectionName, optionName) {
-    return get(this._editing, sectionName, optionName);
-  },
-
-  saveOption: function (sectionName, optionName) {
-    set(this._saving, sectionName, optionName);
-  },
-
-  isOptionSaving: function (sectionName, optionName) {
-    return get(this._saving, sectionName, optionName);
+  saveOption: function (sectionName, optionName, value) {
+    set(this._sections, sectionName, optionName, value);
   },
 
   deleteOption: function (sectionName, optionName) {
-    set(this._deleting, sectionName, optionName);
-  },
-
-  isOptionDeleting: function (sectionName, optionName) {
-    return get(this._saving, sectionName, optionName);
-  },
-
-  deleteOptionValue: function (sectionName, optionName) {
     unset(this._sections, sectionName, optionName);
-  },
-
-  stopOptionDelete: function (sectionName, optionName) {
-    unset(this._deleting, sectionName, optionName);
   },
 
   dispatch: function (action) {
@@ -142,69 +62,13 @@ var ConfigStore = FauxtonAPI.Store.extend({
         this._loading = true;
       break;
 
-      case ActionTypes.EDITING_OPTION:
-        this.editOption(sectionName, optionName);
-      break;
-
-      case ActionTypes.SAVING_OPTION:
-        this.saveOption(sectionName, optionName);
-      break;
-
       case ActionTypes.OPTION_SAVE_SUCCESS:
-        this.setOptionValue(sectionName, optionName, value);
-        this.stopOptionEdit(sectionName, optionName);
-      break;
-
-      case ActionTypes.OPTION_EDIT_CANCEL:
-        this.stopOptionEdit(sectionName, optionName);
-      break;
-
-      case ActionTypes.DELETING_OPTION:
-        this.deleteOption(sectionName, optionName);
+      case ActionTypes.OPTION_ADD_SUCCESS:
+        this.saveOption(sectionName, optionName, value);
       break;
 
       case ActionTypes.OPTION_DELETE_SUCCESS:
-        this.deleteOptionValue(sectionName, optionName);
-      break;
-
-      case ActionTypes.OPTION_DELETE_FAILURE:
-        this.stopOptionDelete(sectionName, optionName);
-      break;
-
-      case ActionTypes.ADDING_OPTION:
-        this._adding = true;
-      break;
-
-      case ActionTypes.OPTION_ADD_SUCCESS:
-        this.setOptionValue(sectionName, optionName, value);
-        this._adding = false;
-        this._addOptionPopoverVisible = false;
-        this._newOptionName = '';
-        this._newOptionValue = '';
-      break;
-
-      case ActionTypes.OPTION_ADD_FAILURE:
-        this._adding = false;
-      break;
-
-      case ActionTypes.TOGGLE_ADD_OPTION_POPOVER:
-        this._addOptionPopoverVisible = !this._addOptionPopoverVisible;
-      break;
-
-      case ActionTypes.HIDE_ADD_OPTION_POPOVER:
-        this._addOptionPopoverVisible = false;
-      break;
-
-      case ActionTypes.UPDATE_NEW_OPTION_NAME:
-        this._newOptionName = optionName;
-      break;
-
-      case ActionTypes.UPDATE_NEW_SECTION_NAME:
-        this._newSectionName = sectionName;
-      break;
-
-      case ActionTypes.UPDATE_NEW_OPTION_VALUE:
-        this._newOptionValue = value;
+        this.deleteOption(sectionName, optionName);
       break;
     }
 
